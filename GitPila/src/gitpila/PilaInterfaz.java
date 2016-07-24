38cf5cd6a574f72a5ba3f7c7aa5b0bf8f17b356b
estebanavalosmorales <estebanavalosmorales@gmail.com>
2016-07-24 12:31:00 -0600

    Add files via upload

--- GitPila/src/gitpila/PilaInterfaz.java
+++ GitPila/src/gitpila/PilaInterfaz.java
@@ -1,14 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+	Esteban Avalos	15059
+	Antonio Ixtecoc	15582
  */
 package gitpila;
 
-/**
- *
- * @author AntonioJose
- */
+
+/*Clase interfaz del programa*/
 public interface PilaInterfaz {
     public void Push(String y);
     public int Pop();
