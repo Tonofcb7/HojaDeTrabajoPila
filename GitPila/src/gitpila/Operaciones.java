57e1be286b8dfa56e273e9484bd89f007dd6f6ec
estebanavalosmorales <estebanavalosmorales@gmail.com>
2016-07-24 12:34:49 -0600

    Add files via upload

--- GitPila/src/gitpila/Operaciones.java
+++ GitPila/src/gitpila/Operaciones.java
@@ -1,34 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+	Esteban avalos 	15059
+	Antonio Ixtecoc 15582
+		
+	Esta Clase es la encargada de realizar las opercaciones  de cada linea
  */
 package gitpila;
 
-/**
- *
- * @author AntonioJose
- */
+
 public class Operaciones {
-    
+    /*Operacion de suma de dos numeros*/
     public int Suma (int x, int y){
         int suma;
         suma= x + y;
         return suma;
     }
-    
+    /*Operacion de resta de dos numeros*/
     public int Resta (int x, int y){
         int resta;
         resta= x-y;
         return resta;
     }
-    
+    /*Operacion de division de dos numeros*/
     public int Division (int x, int y){
         int division;
         division= x/y;
         return division;
     }
-    
+    /*Operacion de multiplicacion de dos numeros*/
     public int Multiplicacion(int x, int y){
         int mul;
         mul= x*y;
