a50f36bc1b312b4f2621d3a7cdcb577600621208
estebanavalosmorales <estebanavalosmorales@gmail.com>
2016-07-24 12:30:23 -0600

    Add files via upload

---
+++ PilaInterfaz.java
@@ -0,0 +1,12 @@
+/*
+	Esteban Avalos	15059
+	Antonio Ixtecoc	15582
+ */
+package gitpila;
+
+
+/*Clase interfaz del programa*/
+public interface PilaInterfaz {
+    public void Push(String y);
+    public int Pop();
+}
